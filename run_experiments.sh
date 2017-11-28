9b26a6bce62084b6d84e473740689d61020112b9
mikigom <sjh14@gist.ac.kr>
2017-11-28 21:43:50 +0900

    updated

--- run_experiments.sh
+++ run_experiments.sh
@@ -38,9 +38,9 @@ python3 trainer.py --Regularization_type LP --Purturbation_type wgan_gp --Lambda
 # Fig8-Top
 python3 trainer.py --Regularization_type GP --Purturbation_type dragan_only_training --Lambda 5.0
 # Fig8-Middle, Fig14-Top
-python3 trainer.py --Regularization_type GP --Purturbation_type dragan_both --Lambda 5.0 --emd_records True
+python3 trainer.py --Regularization_type GP --Purturbation_type dragan_both --Lambda 5.0
 # Fig8-Bottom, Fig14-Bottom
-python3 trainer.py --Regularization_type LP --Purturbation_type dragan_both --Lambda 5.0 --emd_records True
+python3 trainer.py --Regularization_type LP --Purturbation_type dragan_both --Lambda 5.0
 
 
 python3 trainer.py --n_epoch 2000 --Regularization_type GP --Purturbation_type wgan_gp --Lambda 1.0 --emd_records True
